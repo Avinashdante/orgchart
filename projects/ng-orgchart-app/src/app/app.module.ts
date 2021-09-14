009b412d6a03bf4dbc2f5d4e2ae3dc15411b7bc5
Avinashdante <avinash.nani789@gmail.com>
2021-09-15 02:29:39 +0530

    dsad

---
+++ projects/ng-orgchart-app/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OrgchartModule } from '../../../ng-orgchart/src/lib/components/orgchart/orgchart.module'; 
+import { DefaultChartComponent } from './default-chart/default-chart.component';
+import { HomeComponent } from './home/home.component';
+import { CustomNodeTemplateChartComponent } from './custom-node-template-chart/custom-node-template-chart.component';
+import { PanZoomChartComponent } from './pan-zoom-chart/pan-zoom-chart.component';
+import { EditChartComponent } from './edit-chart/edit-chart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DefaultChartComponent,
+    HomeComponent,
+    CustomNodeTemplateChartComponent,
+    PanZoomChartComponent,
+    EditChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    OrgchartModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
